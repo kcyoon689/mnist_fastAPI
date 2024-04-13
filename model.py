61f7fa0333c22be2310b380984e03be0b26d42aa
kcyoon689 <kcyoun689@gmail.com>
2024-04-13 22:30:46 +0900

    edit import

--- model.py
+++ model.py
@@ -1,23 +1,25 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from trainer import MnistModelBase
 from torchsummary import summary
 
-class CNN(nn.Module):
+
+class MnistModel(MnistModelBase):  # MnistModelBase로부터 상속
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels = 1, out_channels = 16, kernel_size=3),   #RF - 3x3 # 26x26
+            nn.Conv2d(
+                in_channels=1, out_channels=16, kernel_size=3
+            ),  # RF - 3x3 # 26x26
             nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.Dropout2d(0.1),
-
-            nn.Conv2d(16, 16, 3),   #RF - 5x5 # 24x24
+            nn.Conv2d(16, 16, 3),  # RF - 5x5 # 24x24
             nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.Dropout2d(0.1),
-
-            nn.Conv2d(16, 32, 3),   #RF - 7x7 # 22x22
+            nn.Conv2d(16, 32, 3),  # RF - 7x7 # 22x22
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.Dropout2d(0.1),
@@ -27,47 +29,44 @@ class CNN(nn.Module):
         # input - 22x22x64; output - 11x11x32
         self.trans1 = nn.Sequential(
             # RF - 7x7
-            nn.Conv2d(32, 20, 1), # 22x22
+            nn.Conv2d(32, 20, 1),  # 22x22
             nn.ReLU(),
             nn.BatchNorm2d(20),
-
             # RF - 14x14
-            nn.MaxPool2d(2, 2), # 11x11
+            nn.MaxPool2d(2, 2),  # 11x11
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(20,20,3,padding=1),   #RF - 16x16 #output- 9x9
+            nn.Conv2d(20, 20, 3, padding=1),  # RF - 16x16 #output- 9x9
             nn.ReLU(),
             nn.BatchNorm2d(20),
             nn.Dropout2d(0.1),
-
-            nn.Conv2d(20,16,3),   #RF - 16x16 #output- 9x9
+            nn.Conv2d(20, 16, 3),  # RF - 16x16 #output- 9x9
             nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.Dropout2d(0.1),
-
-            nn.Conv2d(16, 16, 3),   #RF - 18x18 #output- 7x7
+            nn.Conv2d(16, 16, 3),  # RF - 18x18 #output- 7x7
             nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.Dropout2d(0.1),
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(16,16,3),   #RF - 20x20  #output- 5x5
+            nn.Conv2d(16, 16, 3),  # RF - 20x20  #output- 5x5
             nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.Dropout2d(0.1),
-            #nn.Conv2d(16,10,1),   #RF - 20x20  #output- 7x7
+            # nn.Conv2d(16,10,1),   #RF - 20x20  #output- 7x7
         )
 
         # GAP Layer
         self.avg_pool = nn.Sequential(
             # # RF - 22x22
             nn.AvgPool2d(5)
-        )   ## output_size=1
+        )  ## output_size=1
 
         self.conv4 = nn.Sequential(
-            nn.Conv2d(16,10,1),   #RF - 20x20  #output- 7x7
+            nn.Conv2d(16, 10, 1),  # RF - 20x20  #output- 7x7
         )
 
     def forward(self, xb):
@@ -81,9 +80,10 @@ class CNN(nn.Module):
         x = x.view(-1, 10)
         return x
 
+
 # Print Summary of the model
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = CNN().to(device)
+    model = MnistModel().to(device)
     summary(model, input_size=(1, 28, 28))
     print(model)
