dd4bab6a8da2cbc8724b83a16f6aeae0bc100c4f
kcyoon689 <kcyoun689@gmail.com>
2024-04-13 22:28:52 +0900

    rename dataloader.py to utils.py and add utils (plot, save, load, etc functions)

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
+import torch
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 from torchvision import transforms
 from torch.utils.data import random_split
 from torch.utils.data.dataloader import DataLoader
 import plotly.express as px
+import plotly.graph_objects as go
+import os
+
 
 def get_dataloader(batch_size, val_size):
     dataset = MNIST(root="./data", download=True, transform=ToTensor())
@@ -30,8 +34,8 @@ def get_dataloader(batch_size, val_size):
     train_mean /= len(train_loader.dataset)
     train_std /= len(train_loader.dataset)
 
-    # print("Mean: ", train_mean)
-    # print("Std: ", train_std)
+    # print('Mean: ', train_mean)
+    # print('Std: ', train_std)
 
     dataset = MNIST(
         root="./data",
@@ -53,6 +57,8 @@ def get_dataloader(batch_size, val_size):
         ),
     )
 
+    torch.manual_seed(1)
+
     train_size = len(dataset) - val_size
     train_ds, val_ds = random_split(dataset, [train_size, val_size])
 
@@ -69,11 +75,78 @@ def get_dataloader(batch_size, val_size):
     return train_loader, val_loader, test_loader
 
 
+def makedirs(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+# plot_losses_and_accuracy(history)
+def plotly_plot_losses(train_loss, val_loss):
+    train_losses = train_loss
+    val_losses = val_loss
+
+    fig = go.Figure()
+
+    # Training losses
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, len(train_losses) + 1)),
+            y=train_losses,
+            mode="lines+markers",
+            name="Training",
+        )
+    )
+
+    # Validation losses
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, len(val_losses) + 1)),
+            y=val_losses,
+            mode="lines+markers",
+            name="Validation",
+        )
+    )
+
+    fig.update_layout(
+        title="Loss vs. No. of epochs", xaxis_title="Epoch", yaxis_title="Loss"
+    )
+
+    save_path = "./results/"
+    makedirs(save_path)
+    fig.write_image(os.path.join(save_path, "loss_graph.png"))
+
+
+def plotly_plot_scores(val_acc):
+    # acc = [x['val_acc'] for x in history]
+    acc = val_acc
+
+    fig = go.Figure()
+
+    # Validation accuracy
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, len(acc) + 1)),
+            y=acc,
+            mode="lines+markers",
+            name="Validation Accuracy",
+        )
+    )
+
+    fig.update_layout(
+        title="Accuracy vs. No. of epochs", xaxis_title="Epoch", yaxis_title="Accuracy"
+    )
+
+    # fig.show()
+    save_path = "./results/"
+    makedirs(save_path)
+    fig.write_image(os.path.join(save_path, "accuracy_graph.png"))
+
+
 if __name__ == "__main__":
     train_loader, val_loader, test_loader = get_dataloader(64, 10000)
 
     for images, labels in train_loader:
-        print(images[0].shape, labels[0])
+        # print(images[0].shape, labels[0])
         img = images[0].squeeze().numpy()
         fig = px.imshow(img, color_continuous_scale="gray")
         fig.show()
