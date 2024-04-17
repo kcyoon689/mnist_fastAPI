a4027198982ec15d1cec5a8e2c9d4b0e76ff3b72
kcyoon689 <kcyoun689@gmail.com>
2024-04-18 01:40:37 +0900

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,16 @@
+# python3 -m pip install opencv-python-headless
+python3 -m pip install torch
+python3 -m pip install torchvision
+python3 -m pip install mlflow
+python3 -m pip install plotly
+python3 -m pip install matplotlib
+python3 -m pip install torchsummary
+python3 -m pip install kaleido
+python3 -m pip install mlflow
+python3 -m pip install fastapi
+python3 -m pip install uvicorn
+python3 -m pip install python-multipart
+python3 -m pip install onnx
+python3 -m pip install onnxruntime
+python3 -m pip install lightning
+python3 -m pip install psutil
